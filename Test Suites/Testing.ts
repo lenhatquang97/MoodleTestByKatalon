feffb78fd56010fcd0daf152393c47d72aca6089
Quang <lenhatquang97@gmail.com>
2022-01-16 22:31:26 +0700

    Testing

---
+++ Test Suites/Testing.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9add35c8-ce4e-4b39-84b1-b4a09920417a</testSuiteGuid>
+   <testCaseLink>
+      <guid>8599606c-3fd4-4b8e-81ae-d9fdf647329e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kiểm tra thẻ Site Administration/Đăng nhập thành công</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
